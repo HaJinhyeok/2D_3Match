afa7c77b498228585f64f80b8ed2f4862bdd204a
HaJinhyeok <37235156+HaJinhyeok@users.noreply.github.com>
2025-04-09 15:06:09 +0900

    Initiate

---
+++ Assets/@Scripts/Block.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventSystem))]
+public class Block : MonoBehaviour
+{
+    public Sprite blockImage;
+
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/@Scripts/GameBoard.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GameBoard : MonoBehaviour
+{
+    List<Block> blocks = new List<Block>();
+    Dictionary<GameObject, Block> blockDictionary = new Dictionary<GameObject, Block>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
